4dcc5c4fc69286d4f740bcc30f1d375d71fea2af
Akylzhan Sauranbay <akyl.sauranbai@gmail.com>
2021-03-20 17:58:17 +0600

    show must begin

--- app/routes.py
+++ app/routes.py
@@ -12,29 +12,16 @@ def index():
     form = SurveyForm()
 
     if form.validate_on_submit():
-        print(form.age.data, flush=True)
-
-        flash()
-        return redirect(url_for('index'))
-    return render_template('survey.html', form=form)
-
-
-@app.route("/breast_cancer", methods=["GET", "POST"])
-def breast_cancer():
-    form = BreastCancerForm()
-
-    if form.validate_on_submit():
-        print(form.mean_radius.data, flush=True)
-
-        pred = breast_cancer_predict(form.mean_radius.data,
+        breast_cancer_pred = breast_cancer_predict(form.mean_radius.data,
                                      form.mean_texture.data,
                                      form.mean_perimeter.data,
                                      form.mean_area.data,
                                      form.mean_smoothness.data) #17.99, 10.38, 122.80, 1001.0, 0.11840)
 
-        if pred:
+        if breast_cancer_pred:
             flash("You have breast cancer :C")
         else:
             flash("You don't have breast cancer c:")
-        return redirect(url_for('breast_cancer'))
-    return render_template('breast_cancer.html', form=form)
+
+        return redirect(url_for('index'))
+    return render_template('survey.html', form=form)

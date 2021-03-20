2fcd1fa84b188ac4a716b2cc33f74f3c5722bcec
Akylzhan Sauranbay <akyl.sauranbai@gmail.com>
2021-03-20 18:25:51 +0600

    proper split of forms

--- app/form.py
+++ app/form.py
@@ -5,10 +5,13 @@ from wtforms.widgets import CheckboxInput, ListWidget
 
 
 class SurveyForm(FlaskForm):
+    age = IntegerField("Age")
+    submit = SubmitField("Submit")
+
+
+class BreastCancerForm(FlaskForm):
     mean_radius = FloatField("Mean Radius of Nipple")
     mean_texture = FloatField("Mean Texture of Nipple")
     mean_perimeter = FloatField("Mean Perimeter of Nipple")
     mean_area = FloatField("Mean Area of Nipple")
     mean_smoothness = FloatField("Mean Smoothness")
-
-    submit = SubmitField("Submit")
--- app/routes.py
+++ app/routes.py
@@ -10,18 +10,24 @@ from app.models import *
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     form = SurveyForm()
+    form.breast_cancer_form = BreastCancerForm()
+    print(form.csrf_token.label, flush=True)
 
-    if form.validate_on_submit():
-        breast_cancer_pred = breast_cancer_predict(form.mean_radius.data,
-                                     form.mean_texture.data,
-                                     form.mean_perimeter.data,
-                                     form.mean_area.data,
-                                     form.mean_smoothness.data) #17.99, 10.38, 122.80, 1001.0, 0.11840)
+    if form.breast_cancer_form.validate_on_submit():
+        breast_cancer_pred = breast_cancer_predict(
+                                    form.breast_cancer_form.mean_radius.data,
+                                    form.breast_cancer_form.mean_texture.data,
+                                    form.breast_cancer_form.mean_perimeter.data,
+                                    form.breast_cancer_form.mean_area.data,
+                                    form.breast_cancer_form.mean_smoothness.data) #17.99, 10.38, 122.80, 1001.0, 0.11840)
 
         if breast_cancer_pred:
             flash("You have breast cancer :C")
         else:
             flash("You don't have breast cancer c:")
 
+        return redirect(url_for('index'))
+
+    if form.validate_on_submit():
         return redirect(url_for('index'))
     return render_template('survey.html', form=form)

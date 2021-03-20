08a1d1a138db52e8e814c21b15bccd270ee5f6a3
Akylzhan Sauranbay <akyl.sauranbai@gmail.com>
2021-03-20 23:09:28 +0600

    backend finished

--- app/form.py
+++ app/form.py
@@ -56,3 +56,51 @@ class StrokeForm(FlaskForm):
         (1,"Never smoked"),
         (2,"Yes"),
     ])
+
+
+class HeartAttackForm(FlaskForm):
+    cp = RadioField("Married or was Married?", choices=[
+        (0, "Typical Angina"),
+        (1, "Atypical Angina"),
+        (2, "non-anginal pain"),
+        (3, "asymptomatic")
+    ])
+    trestbps = FloatField("Resting Blood Pressure (mm/Hg)")
+    chol = FloatField("Serum Cholesterol (mg/dL)")
+    fbs = RadioField("Is Fasting Blood Sugar Level > 120 mg/dL?", choices=[
+        (0, "No"), (1, "Yes")
+    ])
+    restecg = RadioField("Resting Electrocardiographic Results?", choices=[
+        (0, "Normal"),
+        (1, "Has ST-T wave abnormality"),
+        (2, "Shows Probability or Definite Left Ventricular Hypertrophy")
+    ])
+    thalach = IntegerField("Maximum heart Rate Achieved")
+    exang = RadioField("Exercise Induced Angina?", choices=[
+        (0, "No"), (1, "Yes")
+    ])
+    oldpeak = IntegerField("ST depression induced by exercise relative to rest")
+    slope = IntegerField("the slope of the peak exercise ST segment")
+    ca = IntegerField("number of major vessels (0-3) colored by flourosopy")
+    thal = IntegerField("?")
+
+
+class DiabetesForm(FlaskForm):
+    Pregnancies=IntegerField("Number of times pregnant")
+    Glucose=IntegerField("Plasma glucose concentration a 2 hours in an oral glucose tolerance test")
+    BloodPressure=IntegerField("Diastolic blood pressure (mm/Hg)")
+    SkinThickness=IntegerField("Triceps skin fold thickness (mm)")
+    Insulin=IntegerField("2-Hour serum insulin (mu U/ml)")
+    BMI=FloatField("Body mass index (weight in kg/(height in m)^2)")
+    DiabetesPedigreeFunction=FloatField("Diabetes pedigree function")
+
+
+class LiverDiseasePredict(FlaskForm):
+    Total_Bilirubin=FloatField("Total Bilirubin (mg/dL)")
+    Direct_Bilirubin=FloatField("Conjugated Bilirubin (mg/dL)")
+    Alkaline_Phosphotase=IntegerField("ALP (IU/L)")
+    Alamine_Aminotransferase=IntegerField("ALT (IU/L)")
+    Aspartate_Aminotransferase=IntegerField("AST (IU/L)")
+    Total_Protiens=FloatField("Total Proteins (g/dL)")
+    Albumin=FloatField("Albumin (g/dL)")
+    Albumin_and_Globulin_Ratio=FloatField("A/G Ratio")
--- app/models.py
+++ app/models.py
@@ -29,3 +29,103 @@ def stroke_predict(gender, age, hypertension, heart_disease, ever_married, work_
     xgb = XGBClassifier()
     xgb.load_model("weights/stroke.model")
     return xgb.predict(data)[0]
+
+
+def heart_attack_predict(age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal):
+    """
+        * Age (age in years)
+        * Sex (1 = male; 0 = female)
+        * CP (chest pain type) (typical angina, atypical angina, non-anginal pain, asymptomatic)
+        * TRESTBPS (resting blood pressure (in mm Hg on admission to the hospital))
+        * CHOL (serum cholesterol in mg/dl)
+        * FBS (fasting blood sugar > 120 mg/dl) (1 = true; 0 = false)
+        * RESTEGC (resting electrocardiographic results)
+        * THALACH (maximum heart rate achieved)
+        * EXANG (exercise induced angina (1 = yes; 0 = no))
+        * OLDPEAK (ST depression induced by exercise relative to rest)
+        * SLOPE (the slope of the peak exercise ST segment)
+        * CA (number of major vessels (0-3) colored by flourosopy)
+        * THAL (3 = normal; 6 = fixed defect; 7 = reversable defect)
+        * RESULT (1 or 0)
+    """
+
+    X = np.array([[int(age),
+                   int(sex),
+                   int(cp),
+                   float(trestbps),
+                   float(chol),
+                   int(fbs),
+                   int(restecg),
+                   int(thalach),
+                   int(exang), 
+                   int(oldpeak),
+                   int(slope),
+                   int(ca),
+                   int(thal)
+                ]])
+    model = pickle.load(open("weights/heart-attack-95.pkl", 'rb'))
+    scaler = pickle.load(open("weights/heart-attack-scaler.bin", 'rb'))
+    X_scaled = scaler.transform(X)
+    result = model.predict(X_scaled)
+    return result[0]
+
+
+def diabetes_predict(Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age):
+    """
+        * Pregnancies: Number of times pregnant
+        * Glucose: Plasma glucose concentration a 2 hours in an oral glucose tolerance test
+        * BloodPressure: Diastolic blood pressure (mm Hg)
+        * SkinThickness: Triceps skin fold thickness (mm)
+        * Insulin: 2-Hour serum insulin (mu U/ml)
+        * BMI: Body mass index (weight in kg/(height in m)^2)
+        * DiabetesPedigreeFunction: Diabetes pedigree function
+        * Age: Age (years)
+    """
+    X = np.array([[int(Pregnancies),
+                   int(Glucose),
+                   int(BloodPressure),
+                   int(SkinThickness),
+                   int(Insulin),
+                   float(BMI),
+                   float(DiabetesPedigreeFunction),
+                   int(Age)
+                ]])
+    model = pickle.load(open("weights/diabetes-87.pkl", 'rb'))
+    scaler = pickle.load(open("weights/diabetes.bin", 'rb'))
+    X_scaled = scaler.transform(X)
+    print(X_scaled)
+    result = model.predict(X_scaled)
+    return result[0]
+
+
+def liver_disease_predict(Age, Total_Bilirubin, Direct_Bilirubin, Alkaline_Phosphotase, Alamine_Aminotransferase, Aspartate_Aminotransferase, Total_Protiens, Albumin, Albumin_and_Globulin_Ratio, Gender):
+    """
+        * Age Age of the patient
+        * Total_Bilirubin Total Bilirubin
+        * Direct_Bilirubin Direct Bilirubin
+        * Alkaline_Phosphotase Alkaline Phosphotase
+        * Alamine_Aminotransferase Alamine Aminotransferase
+        * Aspartate_Aminotransferase Aspartate Aminotransferase
+        * Total_Protiens Total Protiens
+        * Albumin Albumin
+        * Albumin_and_Globulin_Ratio Albumin and Globulin Ratio
+        * Gender Gender of the patient
+    """
+
+    X1 = np.array([[int(Age),
+                    float(Total_Bilirubin),
+                    float(Direct_Bilirubin),
+                    int(Alkaline_Phosphotase),
+                    int(Alamine_Aminotransferase),
+                    int(Aspartate_Aminotransferase),
+                    float(Total_Protiens),
+                    float(Albumin),
+                    float(Albumin_and_Globulin_Ratio)]])
+    X2 = np.array([[int(Gender)]])
+    model = pickle.load(open("liver-disease-70.pkl", 'rb'))
+    scaler = pickle.load(open("liver-disease-scaler.bin", 'rb'))
+    X1 = scaler.transform(X1)
+
+    X = np.hstack((X1, X2))
+    result = model.predict(X)
+    return result[0]
